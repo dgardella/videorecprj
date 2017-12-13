745c878e4e9800527df5ad1ae5bea4286f22017a
Diegog <dgardella@gmail.com>
2017-12-13 16:55:00 -0500

    v0

---
+++ grabar_10min.sh
@@ -0,0 +1,11 @@
+video_prefix=mama-cam1
+video_interval=300
+echo " Recording Video / interval : $video_interval Segs"
+while true;
+do
+video_name=`date +%d%m%Y_%H%M`
+echo "Recording Video name : $video_name"
+cvlc "rtsp://admin:20160404@181.45.72.255:554/onvif2" --sout file/ts:$video_prefix_$video_name.mp4 --stop-time=$video_interval vlc://quit
+#echo $video_prefix-$video_name
+sleep 1
+done
